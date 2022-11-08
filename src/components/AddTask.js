09614951d0d09125902cf7036f391407462ee413
Taylor Pangilinan <taylorpa04@gmail.com>
2022-11-07 17:59:58 -0700

    Add files via upload

---
+++ src/components/AddTask.js
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+
+
+const AddTask = ({ onAdd }) => {
+
+    const [text, setText] = useState('')
+    const [date, setDate] = useState('')
+    const [reminder, setReminder] = useState(false)
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+        if(!text) {
+            alert('Please enter a task')
+            return
+        }
+
+        onAdd({text, date, reminder})
+
+        setText('')
+        setDate('')
+        setReminder(false)
+    }
+
+  return (
+    <form className='add-form' onSubmit={onSubmit}>
+        <div className='form-control'>
+            <label>Task</label>
+            <input type='text' 
+                placeholder='Add Task' 
+                value={text} 
+                onChange={(e) => setText(e.target.value)}
+            />
+        </div>      
+        <div className='form-control'>
+            <label>Day & Time</label>
+            <input 
+                type='text' 
+                placeholder='Day & Time'
+                value={date} 
+                onChange={(e) => setDate(e.target.value)}
+             />
+        </div>      
+        <div className='form-control form-control-check'>
+            <label>Set Reminder</label>
+            <input 
+                type='checkbox'
+                checked={reminder}
+                value={reminder}
+                onChange ={(e) => setReminder(e.currentTarget.checked)}  />
+        </div>      
+        <input type = 'submit' className='btn btn-block' value='Submit Task' />
+    </form>
+  )
+}
+
+export default AddTask
